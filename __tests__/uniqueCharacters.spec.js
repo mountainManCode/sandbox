1276c76150b23604c5cd8222cc2be6d8f599dd89
Aaron <aa.griff9@gmail.com>
2018-02-21 11:34:03 -0800

    uniqueCharacter function complete and passing tests.

--- __tests__/uniqueCharacters.spec.js
+++ __tests__/uniqueCharacters.spec.js
@@ -3,14 +3,23 @@ const uniqueCharacters = require("../lib/uniqueCharacters.js");
 describe("Finding unique characters", () => {
   describe("when the string is all letters", () => {
     test("should return a single unique letter === 1", () => {
-      const result = uniqueCharacters("abcab");
-      expect(result).toEqual("c");
+      const result = uniqueCharacters("dddeeefggghhh");
+      expect(result).toEqual("f");
     });
   });
   describe("when the whole string is made up of the same letter", () => {
     test("should return undefined", () => {
-      const result = uniqueCharacters("aaaa");
+      const result = uniqueCharacters("ttttt");
       expect(result).toEqual("undefined");
     });
   });
+  describe("when there are 2 unique character", () => {
+    test("should return first unique character", () => {
+      const result = uniqueCharacters("terfg");
+      expect(result).toEqual("t");
+    });
+  });
+  // describe("when there is a number in the string", () => {
+  //   test("should return error")
+  // })
 });
